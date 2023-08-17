5b5b3c39a3b6acedfe82fb7f321c635a51508ed1
GLEF1X <glebgar567@gmail.com>
2023-08-16 23:01:22 -0400

    add parameters for `transaction` context manager

--- src/services/base_service.py
+++ src/services/base_service.py
@@ -5,6 +5,9 @@ from contextlib import asynccontextmanager
 
 from motor.motor_asyncio import AsyncIOMotorClient
 from motor.motor_asyncio import AsyncIOMotorClientSession
+from pymongo import ReadPreference
+from pymongo import WriteConcern
+from pymongo.read_concern import ReadConcern
 
 
 class BaseService:
@@ -15,9 +18,15 @@ class BaseService:
     @asynccontextmanager
     async def transaction(
         self,
+        read_concern: ReadConcern | None = None,
+        write_concern: WriteConcern | None = None,
+        read_preference: ReadPreference | None = None,
+        max_commit_time_ms: float | None = None,
     ) -> AbstractAsyncContextManager[AsyncIOMotorClientSession]:
         async with await self.__db_client.start_session() as s:
-            async with s.start_transaction():
+            async with s.start_transaction(
+                read_concern, write_concern, read_preference, max_commit_time_ms
+            ):
                 self._current_session = s
                 yield s
                 self._current_session = None
